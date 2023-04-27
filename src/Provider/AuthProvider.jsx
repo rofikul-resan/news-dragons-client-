50aaad4b0e8e7abbf2ae8b4dbd826249f132ad63
rofikul-resan <resan6231@gmail.com>
2023-04-27 13:35:24 +0600

    add auth context

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,23 +1,37 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import {
   createUserWithEmailAndPassword,
   getAuth,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
 } from "firebase/auth";
 import firebaseApp from "../firebase/firebase.config";
 
-const AuthContext = createContext();
+export const AuthContext = createContext();
 const auth = getAuth(firebaseApp);
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const updateUser = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+        setLoading(false);
+      }
+    });
+    return () => updateUser();
+  }, []);
   const login = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
   const singUp = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
   const authValue = {
+    loading,
     user,
     setUser,
     login,
--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { useContext } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
 
 const PrivateRouter = ({ children }) => {
-  return children;
+  const { user, loading } = useContext(AuthContext);
+  if (loading) {
+    return <p>Loading ............</p>;
+  } else {
+    if (user) {
+      return children;
+    }
+  }
 };
 
 export default PrivateRouter;
